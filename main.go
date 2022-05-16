28f8a837ed706d3fd50b5fc243912f102fbb29c6
mpapenbr <Markus.Papenbrock+github@gmail.com>
2022-05-16 13:01:43 +0000

    chg: back to release events

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 
 	"github.com/mpapenbr/iracelog-release-updater/releaseupdater"
@@ -16,7 +14,7 @@ func main() {
 	if err != nil {
 		log.Fatal("Could not read config")
 	}
-	probot.HandleEvent("create", func(ctx *probot.Context) error {
+	probot.HandleEvent("create_ooo", func(ctx *probot.Context) error {
 		// Because we're listening for "release" we know the payload is a *github.ReleaseEvent
 		event := ctx.Payload.(*github.CreateEvent)
 		if *event.RefType == "tag" {
@@ -36,13 +34,6 @@ func main() {
 	})
 
 	probot.HandleEvent("release", func(ctx *probot.Context) error {
-		// Because we're listening for "release" we know the payload is a *github.ReleaseEvent
-		event := ctx.Payload.(*github.ReleaseEvent)
-		log.Printf("Currently ignoring release events (%s)\n", *event.Action)
-		return nil
-	})
-
-	probot.HandleEvent("release_ooo", func(ctx *probot.Context) error {
 
 		// Because we're listening for "release" we know the payload is a *github.ReleaseEvent
 		event := ctx.Payload.(*github.ReleaseEvent)
@@ -51,23 +42,7 @@ func main() {
 			// log.Printf("got release published %+v\n", event)
 			log.Printf("got release published from %s\n", *event.GetRepo().Name)
 			releaseupdater.ProcessNewRelease(config, ctx, event)
-			repoOwner := "mpapenbr"
-			repo := "demo_deploy1"
-			fileRef := "versions.properties"
-			content, _, resp, err := ctx.GitHub.Repositories.GetContents(context.Background(), repoOwner, repo, fileRef, &github.RepositoryContentGetOptions{})
-			_, resp, err = ctx.GitHub.Repositories.UpdateFile(context.Background(), repoOwner, repo, fileRef, &github.RepositoryContentFileOptions{
-				Content: []byte("demo_app_version1: " + *event.Release.TagName),
-				Message: github.String("changed by bot for " + *event.Release.TagName),
-				SHA:     github.String(*content.SHA),
-			})
-			if err != nil {
 
-				log.Fatalf("UpdateFileRest %v", err)
-			}
-
-			if false {
-				fmt.Printf("%v", resp)
-			}
 		} else {
 			log.Printf("I'm only interested in published releases.\n")
 		}
--- releaseupdater/process_changes.go
+++ releaseupdater/process_changes.go
@@ -21,22 +21,22 @@ func ProcessNewRelease(config *Config, ctx *probot.Context, release *github.Rele
 			for _, update := range action.Update {
 				repoOwner := *release.Repo.Owner.Login
 				log.Printf("Fetching %s from %s/%s\n", update.File, repoOwner, update.Repo)
-				content, _, resp, err := ctx.GitHub.Repositories.GetContents(context.Background(), repoOwner, update.Repo, update.File, &github.RepositoryContentGetOptions{})
+				content, _, _, err := ctx.GitHub.Repositories.GetContents(context.Background(), repoOwner, update.Repo, update.File, &github.RepositoryContentGetOptions{})
 				if err != nil {
 					log.Printf("error reading source: %+v\n", err)
 					continue
 				}
 				fileContent, _ := b64.StdEncoding.DecodeString(*content.Content)
-				log.Printf("Content: <%s>\n", fileContent)
+				// log.Printf("Content: <%s>\n", fileContent)
 				log.Printf("RegEx: <%s>\n", update.Regex)
-				log.Printf("Resp: %+v\n", *resp)
+				// log.Printf("Resp: %+v\n", *resp)
 				newVersion := ReplaceVersion(fileContent, update.Regex, *release.Release.TagName)
 				if string(newVersion) != string(fileContent) {
 					fmt.Printf("Updating file %s\n", update.File)
-					fmt.Printf("NewContent: <%s>\n", string(newVersion))
+					// fmt.Printf("NewContent: <%s>\n", string(newVersion))
 
 					message := fmt.Sprintf("pkg: Bump %s to %s", *commitComponent, *release.Release.TagName)
-					_, resp, err = ctx.GitHub.Repositories.UpdateFile(context.Background(), repoOwner, update.Repo, update.File, &github.RepositoryContentFileOptions{
+					_, _, err = ctx.GitHub.Repositories.UpdateFile(context.Background(), repoOwner, update.Repo, update.File, &github.RepositoryContentFileOptions{
 						Content: []byte(newVersion),
 						Message: github.String(message),
 						SHA:     github.String(*content.SHA),
